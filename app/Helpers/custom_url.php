64f08f40dd2d06ff107efaf306779a0e4f503bce
Anant Jain <anantjain60@gmail.com>
2020-09-02 17:21:38 +0530

    mcq

--- app/Helpers/custom_url.php
+++ app/Helpers/custom_url.php
@@ -4,5 +4,5 @@ function custom_url($arg)
     if (substr($arg, 0, 1) != "/") {
         $arg = "/".$arg;
     }
-    return "http://localhost/Proj_mx02/public/".$arg;
+    return "http://40.87.96.206:9004/".$arg;
 }
